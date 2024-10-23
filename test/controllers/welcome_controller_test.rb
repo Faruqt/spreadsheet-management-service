be9b824cfe21c0399d61537e6b2b9e57139b0c96
Faruqt <faruqabdulsalam@yahoo.com>
2024-10-23 23:50:41 +0100

    test: add test for welcome route

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,13 @@ require "test_helper"
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get welcome_index_url
+    # Get the root URL
+    get root_url
+
+    # Assert that the response was successful
     assert_response :success
+    
+    # Assert that the response body contains the welcome text
+    assert_select "h1", "Welcome to the sheet management service"
   end
 end
